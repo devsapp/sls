6a69e25f814fdcbab875299a1a0fac7749a86e48
wss-git <wssgryx@163.com>
2021-11-16 11:28:27 +0800

    build 0.0.19

--- dist/sls.d.ts
+++ dist/sls.d.ts
@@ -3,12 +3,14 @@ import { IProperties, ICredentials } from './interface';
 export default class Sls {
     logger: ILogger;
     logClient: any;
+    checkPutLog: boolean;
     private stdoutFormatter;
-    constructor(regionId: any, profile: ICredentials);
+    constructor(regionId: any, profile: ICredentials, checkPutLog: boolean);
     checkProjectExist(project: string): Promise<boolean>;
     checkLogStoreExist(project: string, logstore: string): Promise<boolean>;
     createProject(project: string, description: string): Promise<void>;
     createLogStore(project: string, logstore: string, createLogstoreOptions: any): Promise<void>;
+    postLogStoreLogs(project: string, logstore: string, data: any): Promise<void>;
     updateLogStore(project: string, logstore: string, logstoreOptions: any): Promise<void>;
     makeLogstoreIndex(project: string, logstore: string): Promise<void>;
     makeLogstore(project: any, logstore: any, logstoreOption: any): Promise<void>;
